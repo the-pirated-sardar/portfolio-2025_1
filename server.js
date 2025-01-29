91ea08fb129b88b51d697db1788c093b64bdae36
Prateek Singh Arora <psarora@sfu.ca>
2025-01-29 12:29:43 -0800

    v1 ready

--- server.js
+++
@@ -1,9 +0,0 @@
-import { createRequestHandler } from "@remix-run/netlify";
-
-// Optional: Add custom context to loaders
-export const handler = createRequestHandler({
-  getLoadContext() {
-    // Example: Pass environment variables or other data
-    return {};
-  },
-});
